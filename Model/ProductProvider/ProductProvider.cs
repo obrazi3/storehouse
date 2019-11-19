3b8cf47a597c14824a5ef29b7df5e4b279571c9f
Pavel-Laptop <1999pavlik@gmail.com>
2019-11-19 19:56:03 +0300

    Implement classes 'ProductProvider', 'ProductProviderOrder'.

---
+++ Model/ProductProvider/ProductProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class ProductProvider
+    {
+        public string Name { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        private List<string> productCategories;
+
+        public ProductProvider()
+        {
+            productCategories = new List<string>();
+        }
+
+        public void AddProductCategory(string category)
+        {
+            productCategories.Add(category);
+        }
+        
+        public void RemoveProductCategory(string category)
+        {
+            productCategories.Remove(category);
+        }
+    }
+}
---
+++ Model/ProductProviderOrder/ProductProviderOrder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class ProductProviderOrder
+    {
+        public ProductProvider Provider { get; set; }
+        public int OrderNumber { get; set; }
+        public int TotalSumOrder { get; private set; }
+        public StatusProviderOrder Status { get; set; }
+        public DateTime OrderDate { get; set; }
+        private List<ProductFromLot> listProducts;
+
+        public ProductProviderOrder()
+        {
+            listProducts = new List<ProductFromLot>();
+        }
+
+        public void AddProduct(ProductFromLot prod)
+        {
+            listProducts.Add(prod);
+            TotalSumOrder += (prod.Price * prod.Lot.QuantityProduct);
+        }
+
+        public void RemoveProduct(ProductFromLot prod)
+        {
+            if (listProducts.Remove(prod))
+                TotalSumOrder -= (prod.Price * prod.Lot.QuantityProduct);
+        }
+    }
+
+    enum StatusProviderOrder
+    { NotPaid, Paid, Received }
+}
